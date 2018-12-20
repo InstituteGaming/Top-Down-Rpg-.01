0af7a29d35b2f85d5ab19bea0ad42c610fb23c91
ktpellec <36304017+ktpellec@users.noreply.github.com>
2018-12-19 23:20:42 -0500

    R-P-G.01

---
+++ TopdownRpg/Assets/Scripts/FaceMouseController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceMouseController : MonoBehaviour {
+
+
+    public float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rot = Quaternion.AngleAxis(angle, Vector3.forward);
+        
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, speed * Time.deltaTime);
+		
+	}
+}
